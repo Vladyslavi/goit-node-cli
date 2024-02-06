ab9826910ce0ba710f82716dff2a41533726309a
Vladyslavi <135136418+Vladyslavi@users.noreply.github.com>
2024-02-06 18:44:55 +0200

    commit

--- index.js
+++ index.js
@@ -14,29 +14,24 @@ program.parse(process.argv);
 
 const argv = program.opts();
 
-// TODO: рефакторити
 const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
         case 'list':
-            // ...
             allContacts = await contacts.listContacts();
             console.table(allContacts);
             break;
 
         case 'get':
-            // ... id
             oneContact = await contacts.getContactById(id);
             console.table(oneContact);
             break;
 
         case 'add':
-            // ... name email phone
             newContact = await contacts.addContact({ name, email, phone });
             console.table(newContact);
             break;
 
         case 'remove':
-            // ... id
             deleteContact = await contacts.removeContact(id);
             console.table(deleteContact);
             break;
